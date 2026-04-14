590633bb0a45b76d0b048ed3e219b5fd97a7d6f3
talk2johnbrooks <talk2johnbrooks@gmail.com>
2026-04-14 17:34:02 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/mywebviewapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mywebviewapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.giverscycle.base44.app")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
